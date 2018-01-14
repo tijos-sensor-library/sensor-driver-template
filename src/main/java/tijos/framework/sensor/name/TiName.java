8decf303814dcc3cf35b1bfa484aa6ff25ebb734
lemon <lemon.yang@tijos.net>
2018-01-14 09:40:34 +0800

    Initial Version

---
+++ src/main/java/tijos/framework/sensor/name/TiName.java
@@ -0,0 +1,13 @@
+package tijos.framework.sensor.name;
+
+/**
+ * Hello world!
+ *
+ */
+public class TiName 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/tijos/framework/sensor/name/TiNameSample.java
@@ -0,0 +1,13 @@
+package tijos.framework.sensor.name;
+
+/**
+ * Hello world!
+ *
+ */
+public class TiNameSample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
